e56688a2cb3bc6bcfcbb0dcb60c0a67ca4e47ae1
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-03-28 02:01:16 +0700

    upload reponsive and function js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,33 @@ document.addEventListener('DOMContentLoaded', () => {
             product.classList.add('active');
         });
     });
+
+    // navbar overlay
+    const barsIcon = document.querySelector('#navbar-icon');
+    const barsClose = document.querySelector('.navbar-close');
+    const barsModal = document.querySelector('.navbar-mobile');
+    const overLay = document.querySelector('#overlay');
+
+    function openModal() {
+        overLay.style.display = 'block';
+        barsModal.style.display = 'flex';
+        barsModal.style.opacity = '1';
+        barsModal.style.transform = 'translateX(0%)';
+    }
+
+    function closeModal() {
+        barsModal.style.transform = 'translateX(100%)';
+        barsModal.style.opacity = '0';
+        overLay.style.display = 'none';
+    }
+
+    function closeOverLine() {
+        overLay.style.display = 'none';
+        barsModal.style.opacity = '0';
+        barsModal.style.transform = 'translateX(100%)';
+    }
+
+    barsIcon?.addEventListener('click', openModal);
+    overLay?.addEventListener('click', closeOverLine);
+    barsClose?.addEventListener('click', closeModal);
 });
